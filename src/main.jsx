5d571d9975ce447f201b07c9f9327df18bc2f4b7
raulcu <raulcomununocc@gmail.com>
2025-08-06 14:01:58 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // <- Importa BrowserRouter
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/pokedex-app"> {/* <- Aquí defines el basename */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
